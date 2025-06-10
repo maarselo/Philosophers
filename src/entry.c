2a38a14afa0ea3b6d515ce66848847dd2bd4ac4c
maarselo <marce@decaos.com>
2025-06-10 14:12:34 +0200

    Final control entry

--- src/entry.c
+++ src/entry.c
@@ -10,31 +10,31 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "philosophers.h"
+
 static int	ft_check_is_digit(char c)
 {
 	return (48 <= c && c <= 57);
 }
 
-int	ft_atoi(char *str)
+uint64_t	ft_atoi(char *str)
 {
-	int	i;
-	int	sign;
-	int	total;
+	int			i;
+	uint64_t	total;
 
 	i = 0;
-	sign = 1;
 	total = 0;
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
 		i++;
 	if (str[i] == '+' || str[i] == '-')
 	{
 		if (str[i] == '-')
-			sign *= -1;
+			return (0);
 		i++;
 	}
 	while (ft_check_is_digit(str[i]))
 		total = total * 10 + (str[i++] - '0');
-	return (total * sign);
+	return (total);
 }
 
 static int	ft_check_numbers(char **argv)
@@ -44,7 +44,7 @@ static int	ft_check_numbers(char **argv)
 	i = 1;
 	while (argv[i])
 	{
-		if (ft_atoi(argv[i]) <= 0)
+		if (ft_atouint64(argv[i]) <= 0)
 			return (1);
 		i++;
 	}
