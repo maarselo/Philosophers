2502480618d12d147f24234e2498d3a078995e2d
maarselo <marce@decaos.com>
2025-11-14 16:06:33 +0100

    feat: add first two struct to data and philos

--- include/philosophers.h
+++ include/philosophers.h
@@ -10,29 +10,60 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
 
-# ifndef PHILOSOPHERS_H
-#  define PHILOSOPHERS_H
-
+# include <stdbool.h>
 # include <stdio.h>
+# include <pthread.h>
 # include <stdlib.h>
 
-typedef enum e_error {
+typedef enum e_erro
+{
 	ARGUMENTS_NUMBER,
 	ARGS_NOT_NUMERIC,
 	NEGATIVE_NUMBERS,
+	CREATING_VARIABLES,
 }	t_error;
+
+typedef struct s_data	t_data;
+
+typedef struct s_philo
+{
+	int				philo_number;
+	bool			fork;
+	pthread_t		pthread;
+	pthread_mutex_t	fork_mutex;
+	struct s_philo			*left_philo;
+	struct s_philo			*right_philo;
+	int				total_meals;
+	unsigned long	last_meal;
+	t_data			*data;
+}	t_philo;
+
+typedef struct s_data
+{
+	int				total_philos;//
+	long			time_to_die;//
+	long			time_to_eat;//
+	long			time_to_sleep;//
+	int				must_meals;//
+	t_philo			philos;
+	pthread_mutex_t	write_mutex;//
+}	t_data;
 
 //entry.c
 int				ft_isspace(char c);
 int				ft_isdigit(char c);
-void			ft_valid_arguments(int argc, char **argv);
-
+int				ft_valid_arguments(int argc, char **argv);
 
 //parser_utils.c
-long		ft_atol(char *str);
+long			ft_atol(char *str);
+
+//free.c
+void			ft_free_data(t_data *data);
 
 //error.c
-void		ft_error_handler(int error);
+int				ft_error_handler(int error);
 
-# endif
+#endif
