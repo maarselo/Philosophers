31106e073c08f3f14299d13abb066079145ce9d1
maarselo <marce@decaos.com>
2025-12-02 12:37:49 +0100

    feat: destoy mutex state to one philo

--- philo/src/free.c
+++ philo/src/free.c
@@ -16,6 +16,7 @@ static void	ft_free_one_thread(t_philo *philo)
 {
 	pthread_join(philo->thread, NULL);
 	pthread_mutex_destroy(&philo->fork);
+	pthread_mutex_destroy(&philo->philo_state_mutex);
 	philo->data = NULL;
 	free(philo);
 }
